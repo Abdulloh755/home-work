1e7d8edd03ea6cfdd040822694160d5524c0be31
фиAbdulloh mamirjanov <abdullohmamirjanov13@gmail.com>
2024-09-24 17:38:30 +0300

    first commit

--- 7 hw.py
+++ 7 hw.py
@@ -4,14 +4,14 @@ with sqlite3.connect('students.db') as con:
     cursor = con.cursor()
     # cursor.execute('''DROP TABLE IF EXISTS eat''')
     cursor.execute('''CREATE TABLE students(
-    id PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(50),
     last_name  VARCHAR(50),
-    hobby VARCHER(100),
+    hobby TEXT NOT NULL (100),
     birth_yearth INT,
     homework_score INT
     )''')
-    cursor.execute('''INSERT INTO students (first_name,last_name,hobby,birth_year,homework_score) VALUES''')
+    cursor.execute('''INSERT INTO students (first_name,last_name,hobby,birth_year,homework_score) VALUES
     ('Ali', 'Nurdinow','football', 2006,8),
     ('Zohridin','Bahridinov','reading',2007,11),
     ('Hamid','Akbarov','Swimming',2007,9)
@@ -21,7 +21,8 @@ with sqlite3.connect('students.db') as con:
     ('Samiy', 'Abduhalilov','Boxing',2008, 9),
     ('Oyaatilla','Usarov','football',2006,8),
     ('Diyor','Abdumominov','Reading',2008,5),
-    ('Sadik','MAvlianov','boxing',2007,9);
+    ('Sadik','MAvlianov','boxing',2007,9)
+    ''')
 
     cursor.execute('''SELECT * FROM student WHERE LENGTH(last_name) > 10''')
     cursor.execute('''UPDATE users SET first_name ='genius'
@@ -30,7 +31,8 @@ with sqlite3.connect('students.db') as con:
     WHERE first_name = 'genius' ''');
     cursor.execute('''DELETE FROM student
     WHERE  id % 2 = 0 ''');
-
+    for i in cursor.fetchall():
+            print(i)
 
 
 
